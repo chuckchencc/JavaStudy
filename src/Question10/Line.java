d694f14dfa88a3fd8b5be41f9e20614dce67f572
Cc <chuck_chencc@163.com>
2018-06-26 19:00:56 +0800

    Update Question10

--- src/Question10/Line.java
+++ src/Question10/Line.java
@@ -2,20 +2,27 @@ package Question10;
 
 public class Line
 {
-//	Point p1=new Point(1,2);
-//	Point p2=new Point(3,4);
+	// Point p1=new Point(1,2);
+	// Point p2=new Point(3,4);
 	private Point start;
 	private Point end;
+
 	public Line()
 	{
-		start=new Point();
-		end=new Point();
+		start = new Point();
+		end = new Point();
 	}
-	
-	public Line(int x1,int y1,int x2,int y2)
+
+	public Line(int x1, int y1, int x2, int y2)
 	{
-		start=new 
+		start = new Point(x1, y1);
+		end = new Point(x2, y2);
 	}
-	
-	
+
+	public Line(Point p1, Point p2)
+	{
+		this.start = p1;
+		this.end = p2;
+	}
+
 }
--- src/Question10/Question10.java
+++ src/Question10/Question10.java
@@ -6,7 +6,18 @@ public class Question10
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
+		// 一个点 Point
+		// 属性: 横坐标x, 纵坐标y
+		//
+		// 一条线 Line
+		// 属性：起点p1, 终点p2
+		//
+		// 试写出Point 和 Line类的定义，并为其添加几个构造方法
 		Point p1=new Point(3,6);
+		Point p2=new Point();
+		Line l1=new Line(1,2,3,4);
+		Line l2=new Line();
+		Line l3=new Line(p1,p2);
 	}
 
 }
